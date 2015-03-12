da2fc58ebf42ac74a6863fb88ded915036425975
Ariq Chowdhury <ariqchowdhury@gmail.com>
2015-03-11 22:01:13 -0700

    added functions to match strings to types

--- src/css/stylesheet.rs
+++ src/css/stylesheet.rs
@@ -21,6 +21,7 @@ pub struct Declaration {
 	property_value : Value,
 }
 
+#[derive(PartialEq)]
 #[allow(dead_code)]
 enum Property {
 	FontSize,
@@ -30,9 +31,10 @@ enum Property {
 
 #[allow(dead_code)]
 enum Value {
-	Size(Unit),
-	Length(Unit),
+	Size(u32, Unit),
+	Length(u32, Unit),
 	ColorValue(Color),
+	Placeholder,
 }
 
 #[allow(dead_code)]
@@ -46,4 +48,35 @@ struct Color {
 	green: u8,
 	blue: u8,
 	alpha: u8,
+}
+
+fn string_to_property(string :&str) -> Option<Property> {
+	match string.as_slice().trim() {
+		"font-size" => Some(Property::FontSize),
+		"line-height" => Some(Property::LineHeight),
+		"color" => Some(Property::Color),
+		_ => None,
+	}
+}
+
+fn string_to_value(string :&str) -> Option<Value> {
+	match string.as_slice().trim() {
+		_ => Some(Value::Placeholder),
+	}
+}
+
+#[test]
+fn test_string_to_property() {
+	let prop = string_to_property(" font-size  ");
+	assert!(prop.is_some());
+	assert!(prop.unwrap() == Property::FontSize);
+
+	let invalid_prop = string_to_property(" sdf");
+	assert!(invalid_prop.is_none());
+}
+
+#[test]
+fn test_string_to_value() {
+	let val = string_to_value("sdlfj");
+	assert!(val.is_some());
 }
