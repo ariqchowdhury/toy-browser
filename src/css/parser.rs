9af4dc523881e4bbe738a69383b8ac00bc2d2061
Ariq Chowdhury <ariqchowdhury@gmail.com>
2015-03-12 21:11:21 -0700

    Changed the parse_declaration to return a vector of decs to provide a better interface to parsing multi line declarations. updated unit tests.

--- src/css/parser.rs
+++ src/css/parser.rs
@@ -2,6 +2,8 @@ use text_parser;
 use dom_tree;
 use super::stylesheet;
 
+//use std::vec;
+
 pub struct CssParser {
 	pub parse: text_parser::TextParser,
 }
@@ -39,14 +41,33 @@ impl CssParser {
 
 	}
 
-	pub fn parse_declaration(&mut self) -> Option<stylesheet::Declaration> {
+	fn add_declaration_if_valid(&mut self, 
+								dec_list: &mut Vec<stylesheet::Declaration>,
+								prop : &mut Option<String>,
+								val : &mut Option<String>) {
+		if prop.is_some() && val.is_some() {
+			let parsed_prop = stylesheet::string_to_property(&prop.clone().unwrap());
+			let parsed_val = stylesheet::string_to_value(&val.clone().unwrap());
+
+			if parsed_prop.is_some() && parsed_val.is_some() {
+				dec_list.push(stylesheet::Declaration { 
+					property_name: parsed_prop.unwrap(), 
+					property_value: parsed_val.unwrap()
+				});	
+			} 								
+		}
+	}
+
+	pub fn parse_declaration(&mut self) -> Vec<stylesheet::Declaration> {
 		
-		let mut prop : Option<String> = None;
+		let mut prop : Option<String> = None;	
 		let mut val : Option<String> = None;
 
-		let mut retval : Option<stylesheet::Declaration> = None;
+		let mut retval : Vec<stylesheet::Declaration> = Vec::new();
 
 		loop {
+
+
 			if self.parse.end_of_string() {
 				break;
 			}
@@ -66,24 +87,16 @@ impl CssParser {
 								';' | '}' => false,
 								_ => true}));
 						}
-						';' | '}' => { 
-							let line_break = self.parse.consume_char();
-							
-							if prop.is_some() && val.is_some() {
-								let parsed_prop = stylesheet::string_to_property(&prop.unwrap());
-								let parsed_val = stylesheet::string_to_value(&val.unwrap());
-
-								if parsed_prop.is_some() && parsed_val.is_some() {
-									retval = Some(stylesheet::Declaration { 
-										property_name: parsed_prop.unwrap(), 
-										property_value: parsed_val.unwrap()
-									});	
-								} else {
-									retval = None;
-								}
-								
-							}
+						'}' => { 
+							self.parse.consume_char();
+							self.add_declaration_if_valid(&mut retval, &mut prop, &mut val);
 							break;
+						}
+						';' => {
+							self.parse.consume_char();
+							self.add_declaration_if_valid(&mut retval, &mut prop, &mut val);
+							prop = None;
+							val = None;
 						}
 						_ => {
 							prop = Some(self.parse.consume_while(|c| match c {
@@ -92,9 +105,7 @@ impl CssParser {
 						}
 					}
 				} // Some
-				None => {
-					retval = None;
-				}
+				None => {}
 			} // match
 		} //loop
 		retval
@@ -107,9 +118,9 @@ fn test_parse_valid_declaration() {
 	let p = text_parser::TextParser::new(dec_text.to_string());
 	let mut css = CssParser::new(p);
 
-	let dec = css.parse_declaration();
-	assert!(dec.is_some());
-	assert!(dec.unwrap().property_name == stylesheet::Property::FontSize);
+	let mut dec = css.parse_declaration();
+	assert_eq!(dec.len(), 1);
+	assert!(dec.pop().unwrap().property_name == stylesheet::Property::FontSize);
 }
 
 #[test]
@@ -119,7 +130,7 @@ fn test_parse_invalid_a_declaration() {
 	let mut css = CssParser::new(p);
 
 	let dec = css.parse_declaration();
-	assert!(dec.is_none());
+	assert!(dec.is_empty());
 }
 
 #[test]
@@ -129,7 +140,7 @@ fn test_parse_invalid_b_declaration() {
 	let mut css = CssParser::new(p);
 
 	let dec = css.parse_declaration();
-	assert!(dec.is_none());
+	assert!(dec.is_empty());
 }	
 
 #[test]
@@ -139,7 +150,7 @@ fn test_parse_invalid_c_declaration() {
 	let mut css = CssParser::new(p);
 
 	let dec = css.parse_declaration();
-	assert!(dec.is_none());
+	assert!(dec.is_empty());
 }	
 
 #[test]
@@ -149,12 +160,9 @@ fn test_parse_valid_multiline_declaration() {
 	let p = text_parser::TextParser::new(dec_text.to_string());
 	let mut css = CssParser::new(p);
 
-	let dec_1 = css.parse_declaration();
-	assert!(dec_1.is_some());
-	assert!(dec_1.unwrap().property_name == stylesheet::Property::FontSize);
-
-	let dec_2 = css.parse_declaration();
-	assert!(dec_2.is_some());
-	assert!(dec_2.unwrap().property_name == stylesheet::Property::LineHeight);
+	let mut dec = css.parse_declaration();
+	assert_eq!(dec.len(), 2);
+	assert!(dec.pop().unwrap().property_name == stylesheet::Property::LineHeight);
+	assert!(dec.pop().unwrap().property_name == stylesheet::Property::FontSize);
 
 }
